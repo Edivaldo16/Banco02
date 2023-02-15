9222f58604cdac07f5f1d518412fe6efce47d36d
Edivaldo Ferreira <edivaldojunior18@gmail.com>
2023-02-15 06:08:33 -0300

    Add files via upload

---
+++ 20230215/teste.java
@@ -0,0 +1,15 @@
+package Programa;
+
+public class teste {
+
+	public static void main(String[] args) {
+		teste02 a = new teste02();
+		GeradorConta b = new GeradorConta();
+		
+		//System.out.println(a.getRandomString(10));
+		//System.out.println(a.getRandomString(10).getClass().getSimpleName());
+		System.out.println(b.getRandomString(7));
+
+	}
+
+}
